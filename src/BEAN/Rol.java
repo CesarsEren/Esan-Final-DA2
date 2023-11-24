ff5bbad3540c62f625b9b1e9399bfb86dc5e87f7
Andy Landaveri <andy.landaveri@d9gestion.local>
2023-11-23 22:02:11 -0500

    Creacion Clase Rol

---
+++ src/BEAN/Rol.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package BEAN;
+
+/**
+ *
+ * @author andy.landaveri
+ */
+public class Rol {
+    
+}
